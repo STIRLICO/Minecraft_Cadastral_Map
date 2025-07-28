648a037db50288e6e91e846bf45ca522b48ebe0e
STIRLICO <selivanovskiypetr@yandex.ru>
2025-07-28 23:10:32 +0300

    Добавил странички и их редактуру(пока не работает)

--- src/main/java/CadastralController.java
+++ src/main/java/CadastralController.java
@@ -16,9 +16,13 @@ public class CadastralController {
 
     private List<Cadastral_Object> Map;
 
-
-
-
+    {
+        try {
+            Map = Cadastral_Object.loadFromJSON();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
     @GET
@@ -55,10 +59,62 @@ public class CadastralController {
         html.append("<!DOCTYPE html>");
         html.append("<html><head><title>Cadastral Map Area "+id+"</title></head>");
         html.append("<body>");
-        html.append("<h1>Участок"+id+"</h1>");
-        html.append("Владелец: "+ Map.get(id - 1).getOwner());
+        html.append("<h1>Участок "+id+"</h1>");
+        html.append("<p>Владелец: "+ Map.get(id - 1).getOwner());
+        html.append("<p>Бывшие владельцы: "+ Map.get(id - 1).getSellers().toString());
+        html.append("<p>Дата покупки: "+ Map.get(id - 1).getPurchase_date());
+        html.append("<p>Цена: "+ Map.get(id - 1).getPrice()+" "+Map.get(id - 1).getCurrency());
+        html.append("<p>Статус: "+ Map.get(id - 1).getStatus());
+        //html.append(Map.get(id - 1).getPicture());
+        html.append("<p><a href=\""+id+"/edit/\" + child.id + \"\">Редактировать</a>");
+        html.append("</body>");
+        return html.toString();
+    }
 
-        html.append("/<body>");
+    @GET
+    @Path("/map/area/{id}/edit")
+    public String editArea(@PathParam("id") int id) {
+        StringBuilder html = new StringBuilder();
+        html.append("<!DOCTYPE html>");
+        html.append("<html><head><title>Cadastral Map Area "+id+"</title></head>");
+        html.append("<body>");
+        html.append("<h1>Участок "+id+"</h1>");
+
+        html.append("<p>Владелец: ");
+        html.append("<form method=\"post\" action=\"/editOwner/" + id + "\">");
+        html.append("<input type=\"text\" name=\"value\" value=\"" + Map.get(id - 1).getOwner() +"\"/>");
+        html.append("<input type=\"submit\"/></form>");
+
+        html.append("<p>Бывшие владельцы: "+ Map.get(id - 1).getSellers().toString());
+        html.append("<form method=\"post\" action=\"/addSeller/" + id + "\">");
+        html.append("<input type=\"text\" name=\"value\" value=\"" + Map.get(id - 1).getOwner() +"\"/>");
+        html.append("<input type=\"submit\"/></form>");
+
+        html.append("<p>Дата покупки: ");
+        html.append("<form method=\"post\" action=\"/editDate/" + id + "\">");
+        html.append("<input type=\"text\" name=\"value\" value=\"" + Map.get(id - 1).getPurchase_date() +"\"/>");
+        html.append("<input type=\"submit\"/></form>");
+
+        html.append("<p>Цена: ");
+        html.append("<form method=\"post\" action=\"/editPrice/" + id + "\">");
+        html.append("<input type=\"text\" name=\"value\" value=\"" + Map.get(id - 1).getPrice() +"\"/>");
+        html.append("<input type=\"submit\"/></form>");
+        html.append("<form method=\"post\" action=\"/editCurrency/" + id + "\">");
+        html.append("<input type=\"text\" name=\"value\" value=\"" + Map.get(id - 1).getCurrency() +"\"/>");
+        html.append("<input type=\"submit\"/></form>");
+
+
+        html.append("<p>Статус: ");
+        html.append("<form method=\"post\" action=\"/editStatus/" + id + "\">");
+        html.append("<input type=\"text\" name=\"value\" value=\"" + Map.get(id - 1).getStatus() +"\"/>");
+        html.append("<input type=\"submit\"/></form>");
+
+        html.append("<p>Картинка ");
+        html.append("<form method=\"post\" action=\"/editPicture/" + id + "\">");
+        html.append("<input type=\"text\" name=\"value\" value=\"" + Map.get(id - 1).getPicture() +"\"/>");
+        html.append("<input type=\"submit\"/></form>");
+
+        html.append("</body>");
         return html.toString();
     }
 }
