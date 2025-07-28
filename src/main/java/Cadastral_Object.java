6271b34ec6f7c9ab47a2705e50149946b1659c43
STIRLICO <selivanovskiypetr@yandex.ru>
2025-07-28 19:09:23 +0300

    Объект карты

---
+++ src/main/java/Cadastral_Object.java
@@ -0,0 +1,9 @@
+public class Cadastral_Object {
+    private int id;
+    private int price; //Цена
+    private String currency; //Тип валюты
+    private String owner; //Владелец
+    private String[] sellers; //Бывшие владельцы
+    private String purchase_date; //Дата покупки
+    private String status; //Статус (продаётся, купленно, стройка, заброшено, бронь, готово)
+}
